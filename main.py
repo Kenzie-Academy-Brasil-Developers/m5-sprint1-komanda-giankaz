22f09cbd37e1c31b1bbdde4d3a3b2d8d453bb3ff
Giankaz <91921192+Giankaz@users.noreply.github.com>
2022-09-25 20:57:41 -0300

    feat: finished komanda

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+from utils import adding_item_to_menu as write_json, menu_reading as read_json
+from management import total_calculator as calculate_tab
+import ujson as json
+
+with open('menu.json', 'r') as test:
+    data = json.load(test)
+    print(data)
+
 if __name__ == "__main__":
-    # Utilize essa área para testes com print
-    ...
+    print('Reading: ')
+    print(read_json())
+    new_item = {"name": "CHURROS DO M5", "price": 5.0}
+    print('Writing: ')
+    print(write_json(new_item))
+
+    table_1 = [{'id': 1, 'amount': 5}, {'id': 19, 'amount': 5}]
+
+    table_2 = [
+        {"id": 10, "amount": 3},
+        {"id": 20, "amount": 2},
+        {"id": 21, "amount": 5},
+        ]
+    print('Total Bill 1:')
+    print(calculate_tab(table_1))
+    print('Total Bill 2:')
+    print(calculate_tab(table_2))
---
+++ utils/__init__.py
@@ -0,0 +1,6 @@
+from .json_handler import menu_reading, adding_item_to_menu
+
+
+
+
+
